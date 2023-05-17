ca8497ac8741dc56a08a6507d6dd3506a4baacb5
samirwattarNTI <samirwattar89@gmail.com>
2023-05-17 15:10:12 +0200

    7

--- random.php
+++ random.php
@@ -1,20 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
+    <link rel="stylesheet" href="mainpage.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="main-page.css">
-    <title>Document</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons|Material+Icons+Outlined"
+    rel="stylesheet">
+    <title>Switter</title>
 </head>
 <body>
     <header>
-        <div class="logo"></div>
-        <nav class="navigation">
-            <a href="#">profile</a>
-            <a href="#">friends</a>
-            <a href="#">search</a>
-        </nav>
+        <div class="nav-links">
+            <a href="">
+                <i class="material-icons-outlined">
+                    something
+                </i>
+            </a>
+            <a href="">
+                <i class="material-icons-outlined">
+                    home
+                </i>
+                <span>HOME</span>
+            </a>
+            <a href="">
+                <i class="material-icons-outlined">
+                    explore
+                </i>
+                <span>Explore</span>
+            </a>
+            <a href="">
+                <i class="material-icons-outlined">
+                    notification
+                </i>
+            </a>
+            <a href="">
+                <i class="material-icons-outlined">
+                    Messages
+                </i>
+            </a>
+            <a href="">
+                <i class="material-icons-outlined">
+                    profile
+                </i>
+            </a>
+        </div>
+
+        <div class="main-account"></div>
     </header>
+
+
 </body>
 </html>
